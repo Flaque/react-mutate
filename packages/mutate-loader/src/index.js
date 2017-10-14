730e1c7e8e683bac321c0fe32e947af17fc66391
Flaqueeau <econrad@zagmail.gonzaga.edu>
2017-10-14 12:09:23 -0700

    Add extra error message

--- packages/mutate-loader/src/index.js
+++ packages/mutate-loader/src/index.js
@@ -13,7 +13,7 @@ const {
   loadMutationsFromDependencies
 } = require("./util.js");
 const path = require("path");
-const async = require("async");
+const errorIf = require("@flaque/error-if");
 
 /**
  * Installs a list of npm modules as "mutations" in a folder called
@@ -41,7 +41,15 @@ function installMutations(modules, enclosingFolder) {
  */
 function loadMutations(enclosingFolder) {
   const here = jetpack.cwd(pathToMutations(enclosingFolder));
+
   const pkg = here.read(PACKAGE_JSON, "json");
+  errorIf(
+    !pkg,
+    `There doesn't seem to be a package.json file. 
+Did you create one at the enclosing folder? 
+FYI: installMutations creates one for you if you don't have one. `
+  );
+
   const node_modules_path = here.dir(NODE_MODULES).cwd();
 
   return new Promise((resolve, reject) => {
